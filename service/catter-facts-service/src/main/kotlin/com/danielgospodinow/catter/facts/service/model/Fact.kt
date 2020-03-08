ff8d33922cc767663e770488d9e9a3ba47e94942
danielgospodinow <danielgospodinow@gmail.com>
2020-03-08 02:33:56 +0200

    Implement CRUD operations for facts and connect to MongoDB through the reactive Mongo driver

--- service/catter-facts-service/src/main/kotlin/com/danielgospodinow/catter/facts/service/model/Fact.kt
+++ service/catter-facts-service/src/main/kotlin/com/danielgospodinow/catter/facts/service/model/Fact.kt
@@ -1,4 +1,9 @@
 package com.danielgospodinow.catter.facts.service.model
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(value = "facts")
 data class Fact(
-        val content: String)
+        @Id var id: String?,
+        var content: String)
