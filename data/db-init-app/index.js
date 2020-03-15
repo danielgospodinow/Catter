5a1db5e0e7d6c4ec246a5accf5a2dae21d91902e
danielgospodinow <danielgospodinow@gmail.com>
2020-03-15 11:20:18 +0200

    Create simple application for transferring the data into the database

---
+++ data/db-init-app/index.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+const readline = require('readline')
+
+const url = "http://localhost:7001/api/fact"
+
+var lineReader = readline.createInterface({
+    input: fs.createReadStream('../facts.json')
+})
+
+lineReader.on('line', line => {
+    if (line.includes("text") || line.includes("fact")) {
+        const data = line.split(":")[1].trim().slice(1, -2)
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                content: data,
+                author: {
+                    name: "Daniel"
+                }
+            })
+        })
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+    }
+})
