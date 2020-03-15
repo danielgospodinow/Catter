c90422daee21b48a1ee94e0c8d1b6e0e6150e8cb
danielgospodinow <danielgospodinow@gmail.com>
2020-03-15 12:54:51 +0200

    Implement an unoptimized random fact retrieve

--- service/catter-facts-service/src/main/kotlin/com/danielgospodinow/catter/facts/service/service/FactService.kt
+++ service/catter-facts-service/src/main/kotlin/com/danielgospodinow/catter/facts/service/service/FactService.kt
@@ -4,6 +4,8 @@ import com.danielgospodinow.catter.facts.service.model.Fact
 import com.danielgospodinow.catter.facts.service.repository.FactRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import kotlin.random.Random
+
 
 @Service
 class FactService(
@@ -11,7 +13,12 @@ class FactService(
 
     fun getFact(factId: String): Mono<Fact> = factRepository.findById(factId)
 
-    fun getRandomFact(): Mono<Fact> = factRepository.findAll().next()
+    fun getRandomFact(): Mono<Fact> {
+        //TODO: Optimize
+        return factRepository.findAll()
+                .collectList()
+                .flatMap { list -> Mono.just(list[Random.nextInt(list.size)]) }
+    }
 
     fun createFact(fact: Fact): Mono<Fact> = factRepository.save(fact)
 
