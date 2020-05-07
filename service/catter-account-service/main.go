b09c7fd22fe94b9e96e2362567c2590fdab84dd4
danielgospodinow <danielgospodinow@gmail.com>
2020-05-07 21:49:14 +0300

    Fix CORS issues in Accounts service and Facts service

--- service/catter-account-service/main.go
+++ service/catter-account-service/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/danielgospodinow/Catter/service/catter-account-service/db"
 	"github.com/danielgospodinow/Catter/service/catter-account-service/models"
 	"github.com/danielgospodinow/Catter/service/catter-account-service/routers"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -20,5 +22,14 @@ func main() {
 
 	http.Handle("/", router)
 	fmt.Println("Server listening at port 8080 ...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	log.Fatal(http.ListenAndServe(":8080", enableCORS(router)))
+}
+
+func enableCORS(router *mux.Router) http.Handler {
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	return handlers.CORS(headersOk, originsOk, methodsOk)(router)
 }
