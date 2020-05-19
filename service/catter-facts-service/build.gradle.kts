02c8c1a32cfdedc2891d5cacc4c0b03d6230e69a
danielgospodinow <danielgospodinow@gmail.com>
2020-05-19 12:23:02 +0300

    Add Jib plugin to Facts service

--- service/catter-facts-service/build.gradle.kts
+++ service/catter-facts-service/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
+	id ("com.google.cloud.tools.jib") version "2.2.0"
 }
 
 group = "com.danielgospodinow"
